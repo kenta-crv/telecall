b7dbabf93f1407e4ffc1a68d26e463a1793caff4
kenta-crv <adp@crold-valieat.jp>
2020-05-21 12:28:39 +0900

    20200521

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   domain: 'ri-plus.jp',
   port: 587,
   user_name: 'info@ri-plus.jp',
-  password: 'z#6FrWYnp',
+  password: 'M#8hgUQXS',
   authentication: 'plain',
   enable_starttls_auto: true
   }
