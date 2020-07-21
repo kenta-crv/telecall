0dd3fdacf93b19e407a11a083dd698281971a92c
kenta-crv <adp@crold-valieat.jp>
2020-07-21 11:11:40 +0900

    20200721

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,20 @@
 module ApplicationHelper
-
   def default_meta_tags
     {
-    	title:"<%= yield(:title) | 株式会社Ri-Plus' %>",
-    	description: "	ITを活用した分析型アポ代行・テレマーケティング代行なら『アポ匠』！ITプログラミング技術と徹底したマーケティングスキルでテレアポ成功事例を多数保有しております。",
-    	keywords: "アポ代行,テレアポ 代行",
+      site: "テレアポ代行『アポ匠』株式会社Ri-Plus",
+      title:"<%= yield(:title) | 株式会社Ri-Plus' %>",
+      description: "ITを活用した分析型アポ代行・テレマーケティング代行なら『アポ匠』！ITプログラミング技術と徹底したマーケティングスキルでテレアポ成功事例を多数保有しております。",
+      keywords: "アポ代行,テレアポ 代行",
       canonical: request.original_url,  # 優先されるurl
-      charset: "UTF-8"
+      charset: "UTF-8",
+      reverse: true,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('favicon.ico'),  rel: 'apple-touch-icon' },
+      ],
+	    canonical: request.original_url  # 優先されるurl
+
     }
   end
 
