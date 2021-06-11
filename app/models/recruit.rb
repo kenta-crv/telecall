8cba906d1ad67c6ac90c6072450cd979b93290b2
kenta-crv <okuyamakenta@gmail.com>
2021-06-11 16:57:37 +0900

    fix

--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -4,13 +4,9 @@ class Recruit < ApplicationRecord
   validates :last_name, {presence: true} #名前
   validates :tel, {presence: true} #電話番号
   validates :mail, {presence: true} #メールアドレス
-  validates :postnumber, {presence: true} #郵便番号
   validates :address, {presence: true} #住所
   validates :select_1, {presence: true} #
-  validates :select_3, {presence: true} #
-  validates :remarks, {presence: true} #
-  validates :other_1, {presence: true}
-  validates :other_2, {presence: true}
+  validates :select_2, {presence: true} #
   validates :one_first_date, {presence: true}
   validates :one_end_date, {presence: true}
   validates :second_first_date, {presence: true}
