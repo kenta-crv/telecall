ce7359b15299ad40c994680fedd4560f8b7284a4
kenta-crv <okuyamakenta@gmail.com>
2021-10-06 12:35:42 +0900

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   domain: 'ri-plus.jp',
   port: 587,
   user_name: 'info@ri-plus.jp',
-  password: 'uK6PrBF$x',
+  password: 'uK6PrBF$xW',
   authentication: 'plain',
   enable_starttls_auto: true
   }
