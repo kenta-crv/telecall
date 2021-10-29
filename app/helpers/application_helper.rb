343ba7a04d44f26977a35d6e86ee9ba13837e0d2
kenta-crv <okuyamakenta@gmail.com>
2021-10-29 14:05:17 +0900

    renuwal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "テレアポ代行『アポ匠』株式会社Ri-Plus",
+      site: "テレアポ代行を業界最高峰のリストで行うなら成功報酬の『アポ匠』",
       title:"<%= yield(:title) | 株式会社Ri-Plus' %>",
-      description: "ITを活用した分析型アポ代行・テレマーケティング代行なら『アポ匠』！ITプログラミング技術と徹底したマーケティングスキルでテレアポ成功事例を多数保有しております。",
-      keywords: "アポ代行,テレアポ 代行",
+      description: "テレアポ代行でITを活用した分析型アポ代行・テレマーケティング代行なら『アポ匠』！ITプログラミング技術と徹底したマーケティングスキルでテレアポ成功事例を多数保有しております。",
+      keywords: "アポ代行,テレアポ 代行,成功報酬",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       reverse: true,
