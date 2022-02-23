4f4b6dfe006457e585855f1ff7a30579c8161b96
kenta-crv <okuyamakenta@gmail.com>
2022-02-23 18:33:28 +0900

    production

--- config/puma.rb
+++ config/puma.rb
@@ -32,4 +32,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
  preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+#plugin :tmp_restart
+
+activate_control_app
