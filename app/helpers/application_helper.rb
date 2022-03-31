b67e2f64ef5cd2e581be0380cb8edb491124aa32
kenta-crv <okuyamakenta@gmail.com>
2022-03-31 13:13:10 +0900

    title fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "完全成果報酬で事業復活支援金を申請なら助成金サポート.jp",
-      title:"<%= yield(:title) | 助成金サポート.jp' %>",
-      description: "完全成果報酬で事業復活支援金を申請なら助成金サポート.jp。30社限定で初期費用無料キャンペーン実施中！",
+      site: "テレアポ代行で最高峰リストに架電！完全成果報酬の『アポ匠』",
+      title:"<%= yield(:title) | テレアポ代行『アポ匠』' %>",
+      description: "テレアポ代行を成果報酬で考えているなら『アポ匠』！ITプログラミングスキルを活用したリストの精度は非常に評判がよく、高いアポ率をお約束します。",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       reverse: true,
