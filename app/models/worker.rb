9ddd3d1bbc6fb939058f3b05cf82d5bfe9eb6732
kenta-crv <okuyamakenta@gmail.com>
2022-06-06 15:27:38 +0900

    fix

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,10 +3,4 @@ class Worker < ApplicationRecord
   validates :last_name, {presence: true} #名前
   validates :tel, {presence: true} #電話番号
   validates :mail, {presence: true} #メールアドレス
-  validates :postnumber, {presence: true} #郵便番号
-  validates :address, {presence: true} #住所
-  validates :bank, {presence: true}
-  validates :store, {presence: true}
-  validates :account_number, {presence: true}
-  validates :transfer_name, {presence: true}
 end
